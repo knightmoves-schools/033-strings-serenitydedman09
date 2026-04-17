708973abff62be63f4d4a335d65155e66b17ec35
serenity <serenitydedman@gmail.com>
2026-04-17 15:16:22 +0000

    updated

---
+++ index.js
@@ -0,0 +1,6 @@
+let firstName = "Brenda";
+let jobTitle = "Cashier"
+let city = "Chicago"
+
+let newJobPost = `${firstName} is a ${jobTitle} in ${city}`;
+document.getElementById("result").innerHTML = newJobPost;
